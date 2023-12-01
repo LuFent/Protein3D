5e3b7eade06d9c2df6c060d52c0ab625bac9037c
ded_mozai <nik.1104.shiryaev@gmail.com>
2023-12-01 15:48:27 +0300

    Modified with BIO and CA skeleton

---
+++ Algorithm/Arcitecture/Bonds.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class Bond(Enum):
+    SINGLE = 1
+    DOUBLE = 2
+    TRIPLE = 3
--- Algorithm/data/Data_Parcer.py
+++ Algorithm/data/Data_Parcer.py
@@ -1,23 +1,22 @@
 from test_tube import HyperOptArgumentParser
 
-
-class HyperParameters:
+class HyperParameters():
     def __init__(self):
-        self.parser = HyperOptArgumentParser(strategy="grid_search")
+        self.parser = HyperOptArgumentParser(strategy='grid_search')
 
-    # @staticmethod
+    #@staticmethod
     def add_parameters(self, root_dir):
-        # параметры файлов
-        self.parser.opt_list(
-            "--data_ext", default=".pdb1", options=[".pdb"], type=str, tunable=True
-        )
-        self.parser.opt_list("--target_ext", default=".csv", type=str, tunable=True)
-        self.parser.opt_list("--data_dir", default="/data", type=str, tunable=True)
-        self.parser.opt_list("--proj_dir", default=root_dir, type=str, tunable=True)
-        self.parser.opt_list("--data_name", default="4hhb", type=str, tunable=True)
 
-        # параметры командной строки
-        # self.parser.add_argument('--data_name', type=str)
-        # self.parser.add_argument('--data_dir', default='./data', type=str)
+        #параметры файлов
+        self.parser.opt_list('--data_ext', default=".pdb", options=[".pdb1"], type=str, tunable=True)
+        self.parser.opt_list('--target_ext', default=".pdb", type=str, tunable=True)
+        self.parser.opt_list('--data_dir', default='/data', type=str, tunable=True)
+        self.parser.opt_list('--proj_dir', default=root_dir, type=str, tunable=True)
+        self.parser.opt_list('--data_name', default='7jo8', type=str, tunable=True)
+
+        #параметры командной строки
+        #self.parser.add_argument('--data_name', type=str)
+        #self.parser.add_argument('--data_dir', default='./data', type=str)
 
         return self.parser
+
