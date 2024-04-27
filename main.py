9e03b8cdf532cdd9894d3a2a7c3bd9c52b73e76f
LuFent <vladmarchenkosteam@gmail.com>
2024-04-27 17:46:12 +0300

    backend structure reworked + residue selection

--- main.py
+++ main.py
@@ -1,140 +1,82 @@
-from flask import Flask, render_template, request, send_file, session, abort, jsonify
-import os
-import atexit
-from flask_cors import CORS
-from tempfile import NamedTemporaryFile
-import secrets
-
-from datetime import timedelta
-
-from algorithms.Protein3D import StructureVisualisation, NotDisordered
+from server import run_server
+from algorithms.Algorithm import Algorithm, AlgorithmsStorage
+from algorithms.Mask import Mask
+from algorithms.Arcitecture import CIHBS, CIHBS_selector
 
 
-app = Flask(__name__)
-CORS(app, supports_credentials=True, expose_headers='Content-Disposition')
-app.secret_key = 'your_secret_key'
-app.config['UPLOAD_FOLDER'] = 'pdb'
-app.config['ALLOWED_EXTENSIONS'] = {'pdb', 'pdb1'}
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=1)
-app.config['TEMPFILE_DIR'] = "tempfiles"
-#app.config.update(SESSION_COOKIE_SAMESITE="None", SESSION_COOKIE_SECURE=True)
-STRUCTURES_STORE = {}
-TEMP_FILES = set()
+class AlphaCarbonChainAlgorithm(Algorithm):
+    code_name = "alpha_carbon_skeleton"
+    label = "Alpha-Carbon Skeleton"
 
 
-os.makedirs(app.config['TEMPFILE_DIR'], exist_ok=True)
+    def _execute(self, structure) -> Mask:
+        carbon_bond_type_number = 4
+        mask = Mask()
+        show_ids = []
+        extra_bonds = []
+
+        previous_atom = None
+
+        for model in structure:
+            # Iterate through each chain in the model
+            for chain in model:
+                # Iterate through each residue in the chain
+                for residue in chain:
+
+                    # Iterate through each atom in the residue
+                    for atom in residue:
+
+                        if atom.get_name() == "CA":
+                            show_ids.append(atom.get_serial_number())
+                            if previous_atom is None:
+                                previous_atom = atom.get_serial_number()
+                            else:
+                                extra_bonds.append(
+                                    (previous_atom, atom.get_serial_number(), carbon_bond_type_number))
+                                previous_atom = atom.get_serial_number()
+                previous_atom = None
+
+        mask.show_atoms = show_ids
+        mask.hide_atoms = None
+        mask.extra_bonds = {"alpha_carbon_skeleton": {
+            "alpha_carbon_skeleton": extra_bonds
+            }
+        }
+        return mask
 
 
-def requires_structure(func):
-    def wrapper(*args, **kwargs):
-        if 'sid' not in session:
-            return jsonify({'error': 'Session does not have the structure'}), 400
-        return func(*args, **kwargs)
+class CIHBSAlgorithm(Algorithm):
+    code_name = "CIHBS"
+    label = "Ion-Hydrogen Bonds System"
 
-    wrapper.__name__ = func.__name__
-    return wrapper
-
-
-def allowed_file(filename):
-    _, file_extension = os.path.splitext(filename)
-    return file_extension in app.config['ALLOWED_EXTENSIONS']
+    def __init__(self):
+        self.cihbs_obj = CIHBS.BaseCIHBS()
+        self.inner_cihbs_obj = CIHBS.InnerCIHBS()
+        self.outer_cihbs_obj = CIHBS.OuterCIHBS()
+        self.physical_cihbs_obj = CIHBS.PhysicalOperators()
+        self.cihbs_selector_obj = CIHBS_selector.SelectorCIHBS()
 
 
-def clean_tempfiles_dir():
-    directory = app.config['TEMPFILE_DIR']
-    for filename in os.listdir(directory):
-        file_path = os.path.join(directory, filename)
-        if os.path.isfile(file_path):
-            os.remove(file_path)
+    def _execute(self, structure) -> Mask:
+        mask = Mask()
+        self.cihbs_obj.setParameters(structure=structure)
+        self.inner_cihbs_obj.checkInnerGroups(structure, self.cihbs_obj.getMainChain())
+
+        self.physical_cihbs_obj.setParameters(args=(self.inner_cihbs_obj.getInnerCIHBSDict(),
+                                                    self.inner_cihbs_obj.getInnerCIHBS(),
+                                                    self.cihbs_obj.getCarbonChain()))
+        self.physical_cihbs_obj.connectPhysicalOperators()
+        new_cihbs = self.physical_cihbs_obj.getNewCIHBS()
+
+        self.outer_cihbs_obj.setParameters(self.inner_cihbs_obj.getInnerCIHBS())
+        self.outer_cihbs_obj.connectResidueCIHBS()
+        new_cihbs.extend(self.outer_cihbs_obj.getNewCIHBS())
+
+        self.cihbs_selector_obj.setParameters(new_cihbs)
+        mask.extra_bonds = self.cihbs_selector_obj.get_selector_map()
+        mask.hide_atoms = None
+        mask.show_atoms = self.inner_cihbs_obj.getInnerCIHBS_serial_number()
+        return mask
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
-
-@app.route("/delete_structure_file", methods=['DELETE'])
-def delete_structure_file():
-    if request.is_json:
-        json_data = request.json
-        if 'file_name' in json_data:
-            file_name = json_data['file_name']
-        else:
-            return jsonify({'error': 'JSON data does not contain the "file_name" key'}), 400
-    else:
-        return jsonify({'error': 'Request data is not JSON'}), 400
-
-    try:
-
-        os.remove(os.path.join(app.config["TEMPFILE_DIR"], file_name))
-    except Exception:
-        pass
-    return "ok", 200
-
-
-@app.route("/upload_structure", methods=['POST'])
-def upload_structure():
-    if 'file' not in request.files:
-        abort(400)
-
-    file = request.files['file']
-
-    if not file:
-        abort(400)
-
-    if session.get("sid", False):
-        session_id = session["sid"]
-        if STRUCTURES_STORE.get(session_id, False):
-            del STRUCTURES_STORE[session_id]
-    else:
-        session_id = secrets.token_hex(16)
-        session['sid'] = session_id
-
-    temp_file = NamedTemporaryFile(delete=True, mode='w+', dir=app.config['TEMPFILE_DIR'])
-    temp_file.write(file.read().decode('utf-8'))
-    temp_file.seek(0)
-    structure = StructureVisualisation.get_structure(session_id, temp_file)
-    temp_file.close()
-    cleaned_temp_file = NamedTemporaryFile(delete=False, mode='w+', dir=app.config['TEMPFILE_DIR'])
-    StructureVisualisation.clean_file(structure, cleaned_temp_file)
-    cleaned_temp_file.seek(0)
-    cleaned_structure = StructureVisualisation(session_id, cleaned_temp_file)
-    STRUCTURES_STORE[session_id] = cleaned_structure
-    response = send_file(cleaned_temp_file.name, as_attachment=True)
-    response.headers["Content-Disposition"] = os.path.basename(cleaned_temp_file.name)
-    cleaned_temp_file.close()
-    TEMP_FILES.add(cleaned_temp_file.name)
-
-    return response
-
-
-@app.route("/exec_algorithm", methods=['POST'])
-@requires_structure
-def exec_algorithm():
-    if request.is_json:
-        json_data = request.json
-        if 'alg' in json_data:
-            alg = json_data['alg']
-        else:
-            return jsonify({'error': 'JSON data does not contain the "alg" key'}), 400
-    else:
-        return jsonify({'error': 'Request data is not JSON'}), 400
-
-    structure = STRUCTURES_STORE[session["sid"]]
-    
-    try:
-        mask = structure.execute_algorithm(alg)
-    except Exception:
-        return jsonify({'error': 'Something went wrong'}), 400
-
-    if mask is None:
-        return jsonify({'error': f'No such algorithm {alg}'}), 400
-    return jsonify(mask), 200
-
-
-
-atexit.register(clean_tempfiles_dir)
-
-if __name__ == "__main__":
-    app.run(host="localhost", port=8000, debug=True)
+run_server(AlphaCarbonChainAlgorithm, CIHBSAlgorithm)
